a635a883a3deef8bc425a02883a85ed62a84f356
mika <20085532d@connect.polyu.hk>
2023-04-18 05:02:48 +0800

    Final version

--- rsa.js
+++ rsa.js
@@ -2,7 +2,6 @@ function generateKeyPair() {
     const rsa = forge.pki.rsa;
     const keyPair = rsa.generateKeyPair({ bits: 2048, e: 0x10001 });
 
-    // 将公钥和私钥转换为 DER 编码格式的 Base64 字符串
     const publicKeyDer = forge.pki.publicKeyToAsn1(keyPair.publicKey);
     const publicKeyDerBytes = forge.asn1.toDer(publicKeyDer).getBytes();
     const publicKeyString = forge.util.encode64(publicKeyDerBytes);
--- rsa_encrypt.js
+++ rsa_encrypt.js
@@ -2,6 +2,8 @@
 function rsaEncrypt(plaintext, publicKey) {
         const jsEncrypt = new JSEncrypt();
         jsEncrypt.setPublicKey(publicKey);
+        const maxEncryptLength = Math.floor(jsEncrypt.getKey().n.bitLength() / 8) - 11;
+        console.log("maxEncryptLength: "+maxEncryptLength);
         const encryptedAesKey = jsEncrypt.encrypt(plaintext);
         return encryptedAesKey;
     }
